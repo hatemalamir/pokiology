316b61a1e5d2c99fa2f1add24c4ad0adc196f7a8
HatemAlamir90 <hatem.alamir.90@gmail.com>
2021-12-13 10:51:15 -0600

    Eda. Printing variables

--- pokimon_classifier.py
+++ pokimon_classifier.py
@@ -8,10 +8,38 @@ def run_classifier(pokimon):
     Input
         pokimon: json object with pokimon properties
     '''
+    print(f'>>> Preprocessing pokimon: {pokimon["name"]} ...')
+    pok_attributes, pok_true_classes = preprocess(pokimon)
+    print(f'<<< Preprocessing pokimon: {pokimon["name"]} done!\n')
+
     print(f'>>> Classifying pokimon: {pokimon["name"]} ...')
-    pok_class = None
-    print(f'Pokimon: {pokimon["name"]} ---> Class: {pok_class}')
-    return pok_class
+    pok_classes = random_forest_classifier(pok_attributes)
+    print(f'<<< Predicted class: {pok_classes}, true classes: {pok_true_classes}\n')
+    return pok_classes
+
+
+def preprocess(pokimon_raw):
+    pokimon_attributes = {
+        'base_experience': pokimon_raw['base_experience'],
+        'height': pokimon_raw['height'],
+        'weight': pokimon_raw['weight'],
+        'order': pokimon_raw['order'],
+    }
+    # TODO: extract the rest of useful attribute
+    # TODO: encode categorical variables
+
+    pokimon_true_classes = []
+    for pok_type in pokimon_raw['types']:
+        pokimon_true_classes.append(pok_type['name'])
+    # TODO: vector-encode classes
+
+    return pokimon_attributes, pokimon_true_classes
+
+
+def random_forest_classifier(pokimon_attributes):
+    pok_classes = None
+    # TODO: build (or load) classifier from training data
+    return pok_classes
 
 
 if __name__ == '__main__':
